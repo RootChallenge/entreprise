e82d53ff057b06ab66fa879338ac630b4c30d453
cnzengue <cnzengue@gmail.com>
2017-04-10 12:27:13 +0100

    crud produit

---
+++ application/models/Categorie_produit_model.php
@@ -0,0 +1,16 @@
+<?php
+	class Categorie_produit_model extends MY_Model
+	{
+		protected $_table = 'categorie_produit';
+		protected $primary_key = 'categorie_produit_id';
+
+		
+		public function __construct()
+		{
+			parent::__construct();
+		}
+		
+		
+	}
+?>
+ 
